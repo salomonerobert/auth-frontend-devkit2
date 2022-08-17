dd6aeb38200f03e3660dbf9a740aa06b88a6fce7
Salomone <darsalomone@gmail.com>
2022-08-17 10:00:05 +0800

    added database operations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect } from "react";
 import { ContextHolder } from "@frontegg/rest-api";
@@ -16,6 +15,56 @@ function App() {
     const baseUrl = ContextHolder.getContext().baseUrl;
     window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
   };
+  const checkAndAddCustomerToDB = () => {
+    var requestOptions = {
+      method: "GET",
+    };
+
+    fetch(
+      "http://localhost:3000/customer/checkandadd?cemail=" +
+        user?.email +
+        "&cname=" +
+        user?.name,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log("Error for checking and adding: ", error));
+  };
+  const getOrdersFromServer = () => {
+    checkAndAddCustomerToDB();
+
+    var requestOptions = {
+      method: "GET",
+    };
+
+    fetch(
+      "http://localhost:3000/order/customer?cemail=" + user?.email,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        var text = `<h4>Welcome ${user?.name}! Here are the items you have ordered:</h4>`;
+        if (data.length === 0) {
+          text += `<h5>No items found in order list</h5>`;
+        } else {
+          text += `<ul class="orderlist">`
+          data.forEach(function (item) {
+            text += `<li class="orderlist">
+            <strong>Item Name:</strong> ${item.name} <br>
+            <strong>Quantity:</strong> ${item.quantity} <br>
+            <strong>Shipping Date:</strong> ${item.shipping_date}
+            </li> <hr>`;
+          });
+          text += "</ul>";
+        }
+        document.getElementById("mypanel").innerHTML = text;
+      })
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <div className="App">
       {isAuthenticated ? (
@@ -31,6 +80,10 @@ function App() {
             <button onClick={() => logout()} className="button">
               Click to logout
             </button>
+            
+          </div>
+          <div className="card" id="mypanel">
+            {getOrdersFromServer()}
           </div>
         </div>
       ) : (
